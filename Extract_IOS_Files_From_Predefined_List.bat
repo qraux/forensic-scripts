d87aecb02aef161a526001c6b3ed357c6cdc7fd8
qraux <22980390+qraux@users.noreply.github.com>
2022-05-05 14:19:22 +0200

    v1.2

--- Extract_IOS_Files_From_Predefined_List.bat
+++ Extract_IOS_Files_From_Predefined_List.bat
@@ -1,15 +1,20 @@
 @echo off
 REM Drag and drop an IOS FFS zip on the bat file and defined files will be extracted.
 REM Just add or delete file paths in the files variable to modificate the outcome.
-REM Version 1.1
+REM Version 1.2
 
+set files[0]=/private/var/mobile/media/PhotoData/Photos.Sqlite
+set files[1]=/private/var/mobile/media/PhotoData/Photos.Sqlite-shm
+set files[2]=/private/var/mobile/media/PhotoData/Photos.Sqlite-wal
 set PathTo7zip=C:\Program Files\7-Zip\7z.exe
-set "files=/private/var/mobile/media/PhotoData/Photos.Sqlite /private/var/mobile/media/PhotoData/Photos.Sqlite-shm /private/var/mobile/media/PhotoData/Photos.Sqlite-wal"
 
-FOR %%a IN (%files%) DO (
-"C:\Program Files\7-Zip\7z.exe" x "%~1" -o"_Extracted_" %%a
+setlocal enabledelayedexpansion
+
+FOR /L %%a IN (0,1,2) DO (
+"C:\Program Files\7-Zip\7z.exe" x "%~1" -o"_Extracted_" !files[%%a]!
 )
 
 pause
 exit
+
 
